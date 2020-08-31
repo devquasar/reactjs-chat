3cf3a467693712bdc036008c570a63e51c8ae439
rkz1337 <booster2228@yandex.ru>
2020-08-31 23:23:16 +0300

    Home page WIP

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,24 @@ import "./Home.scss";
 const Home = () => {
   return (
     <section className="home">
-      <Message
+      {/* <Dialogs
+        items={[
+          {
+            user: {
+              fullname: "Фёдор Достоевский",
+              avatar: null,
+            },
+            message: {
+              text:
+                "Мы все свидетельствуем Вам глубочайшее наше почтение и целуем Ваши ручки, дражайший папенька: Михайла, Федор, Варвара и Андрюша.",
+              isReaded: false,
+              created_at: new Date(),
+            },
+          },
+        ]}
+      /> */}
+
+      {/* <Message
         avatar="https://source.unsplash.com/100x100/?random=1&nature,water"
         text="Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝"
         date={new Date(2020, 7, 29, 22, 11, 0)}
@@ -33,6 +50,19 @@ const Home = () => {
         isMe={true}
         isReaded={false}
       ></Message>
+      <Message
+        avatar="https://source.unsplash.com/100x100/?random=6&nature,water"
+        attachments={[
+          {
+            filename: "image.jpg",
+            url: "https://source.unsplash.com/100x100/?random=7&nature,water",
+          },
+        ]}
+      ></Message>
+      <Message
+        avatar="https://source.unsplash.com/100x100/?random=6&nature,water"
+        isTyping
+      ></Message> */}
     </section>
   );
 };
